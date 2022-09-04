4ff7a616c090f849718c2c27db4da16c6d7b533e
sara-montasser <saramontasser4@gmail.com>
2022-09-04 21:19:39 +0200

    fix: Fix the mentioned issues

--- script.js
+++ script.js
@@ -1,42 +1,26 @@
 let courses;
 let  coursesContainer = document.getElementById("courseContainer");
 function renderhtml(course) {
+  let str=`
+  <div class="course1">
+  <img src=${course.image} alt="python" class="pythonimg">
+  <a class="pythoncourse">${course.title}</a>
+  <h7>${course.author}</h7>
+  <p class="ratingsection">${course.rate}</p>
+  <i class="fa fa-star checked"></i>
+  <i class="fa fa-star checked"></i>
+  <i class="fa fa-star checked"></i>
+  <i class="fa fa-star checked"></i>
+  <i class="fa fa-star-half-full notchecked"></i>
+  <p class="visiting">${course.reviewers}</p>
+  <p class="money">${course.price}<s>${course.oldPrice}</s></p>
+  </div>
+  `;
+  
   console.log(course);
-  let coursedata = document.createElement("div"), img = document.createElement("img"), courseName = document.createElement("a");
-  let authorName = document.createElement("h7"), rating = document.createElement("p"), star = document.createElement("i"), star1 = document.createElement("i"), star2 = document.createElement("i");
-  let star3 = document.createElement("i"), star4 = document.createElement("i"), star5 = document.createElement("i");
-  let viewers = document.createElement("p"), price = document.createElement("p"), oldPrice = document.createElement("s");
+  let coursedata = document.createElement("div");
   coursedata.classList.add("course1");
-  img.src = course.image;
-  img.alt = "python";
-  img.classList.add("pythonimg");
-  coursedata.appendChild(img);
-  courseName.innerHTML = course.title;
-  courseName.classList.add("pythoncourse");
-  coursedata.appendChild(courseName);
-  authorName.innerHTML = course.author;
-  coursedata.appendChild(authorName);
-  rating.innerHTML = course.rate;
-  rating.classList.add("ratingsection");
-  coursedata.appendChild(rating);
-  star.classList.add("fa", "fa-star", "checked");
-  coursedata.appendChild(star);
-  star2.classList.add("fa", "fa-star", "checked");
-  coursedata.appendChild(star2);
-  star3.classList.add("fa", "fa-star", "checked");
-  coursedata.appendChild(star3);
-  star4.classList.add("fa", "fa-star", "checked");
-  coursedata.appendChild(star4);
-  star5.classList.add("fa", "fa-star-half-full", "notchecked");
-  coursedata.appendChild(star5);
-  viewers.innerHTML = course.reviewers;
-  viewers.classList.add("visiting");
-  coursedata.appendChild(viewers);
-  price.innerHTML = course.price;
-  price.classList.add("money");
-  oldPrice.innerHTML = course.oldPrice;
-  price.appendChild(oldPrice);
-  coursedata.appendChild(price);
+  coursedata.innerHTML=str;
   coursesContainer.appendChild(coursedata);
 }
 function getCourses() {
